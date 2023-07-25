618f24ed99e9ccc1804ef0f24e65838577982765
Pazdikan <pazdikan@gmail.com>
2023-07-26 00:43:55 +0200

    Remove meme text w/IP info on failed login page.

--- src/modules/auth/discord.js
+++ src/modules/auth/discord.js
@@ -3,7 +3,6 @@ const { Router } = require("express");
 const config = require("../../settings");
 const router = Router();
 const log = require("../logger/logger");
-const axios = require('axios');
 
 router.get("/discord", (req, res) => {
   const oauth2 = new DiscordOauth2();
@@ -31,28 +30,22 @@ router.get("/discord/callback", async (req, res) => {
     const user = await oauth2.getUser(tokenData.access_token);
 
     if (config.authentication.permitted.includes(user.email)) {
-      log.warn(`${user.username} (${user.id}) logged in using ${user.email} from ${req.ip}`);
+      log.warn(
+        `${user.username} (${user.id}) logged in using ${user.email} from ${req.ip}`
+      );
       req.session.user = user;
 
       const redirectTo = req.session.redirectTo || "/";
       res.redirect(redirectTo);
-      
     } else {
-      log.warn(`${user.username} (${user.id}) tried to log in using ${user.email} from ${req.ip} (unauthorized email)`);
-
-      let geoloc
-      try {
-        let response = await axios.get(`http://ip-api.com/json/${req.ip.replace('::ffff:', '')}?fields=country,regionName,city,isp,asname`);
-        geoloc = response.data;
-      } catch (err) {
-        log.error(err);
-      }
+      log.warn(
+        `${user.username} (${user.id}) tried to log in using ${user.email} from ${req.ip} (unauthorized email)`
+      );
 
       return res.render("shitpost", {
         reason: 401,
         settings: config,
-        ip: geoloc || {}
-      })
+      });
     }
   } catch (err) {
     log.error(err);
